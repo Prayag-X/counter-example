6cb0056eeef71306f9e6573f0c918c21a6564507
DragonNexa <tirtha070@gmail.com>
2025-07-26 11:38:57 +0530

    set up the architechture

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,15 +8,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Counter", style: TextStyle(color: Colors.white),)),
-          backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            "Counter",
+            style: TextStyle(color: Colors.white),
+          ),
         ),
+        backgroundColor: Colors.blue,
+      ),
       body: Center(
-        child: Text("HELLO"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "$count",
+              style: TextStyle(fontSize: 60),
+            ),
+            Container(
+              width: 100,
+              height: 40,
+              color: Colors.blue,
+              child: Center(
+                child: Text(
+                  "Increment",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
